6c0bf83ce637843004db90b508bb740f0e1507f7
Andrey Konoplev <konoplev@wamba.com>
2019-12-09 15:50:07 +0300

    16 variant finish

--- Nastya_Konopleva_16v/Nastya_Konopleva_16v/GroupClass.cpp
+++ Nastya_Konopleva_16v/Nastya_Konopleva_16v/GroupClass.cpp
@@ -16,30 +16,30 @@ GroupClass::GroupClass() {
 }
 
 //Иннициализаторы
-GroupClass::GroupClass(string surname, string university_name, string faculty, int course) {
-    surname = surname;
-    university_name = university_name;
-    faculty = faculty;
-    course = course;
+GroupClass::GroupClass(string sname, string universityName, string faculties, int course1) {
+    surname = sname;
+    university_name = universityName;
+    faculty = faculties;
+    course = course1;
 }
 
 GroupClass::~GroupClass() {}
 
 //Сеттеры свойств (Задают закрытым свойствам значения)
-void GroupClass::setSurname(string surname) {
-    surname = surname;
+void GroupClass::setSurname(string surnameArg) {
+    surname = surnameArg;
 };
 
-void GroupClass::setUniversityName(string name) {
-    university_name = name;
+void GroupClass::setUniversityName(string nameArg) {
+    university_name = nameArg;
 };
 
-void GroupClass::setFaculty(string faculty) {
-    faculty = faculty;
+void GroupClass::setFaculty(string facultyArg) {
+    faculty = facultyArg;
 };
 
-void GroupClass::setCourse(int course) {
-    course = course;
+void GroupClass::setCourse(int courseArg) {
+    course = courseArg;
 };
 
 //Геттеры (Получить значения закрытых свойств)
--- Nastya_Konopleva_16v/Nastya_Konopleva_16v/main.cpp
+++ Nastya_Konopleva_16v/Nastya_Konopleva_16v/main.cpp
@@ -8,11 +8,91 @@
 
 #include <iostream>
 #include "GroupClass.hpp"
+#include <cstring>
 
 int main(int argc, const char * argv[]) {
 
+    //1.Cоздание обьекта с помощью конструктора по умолчанию
+    GroupClass group;
+    group.setSurname("Konopleva");
+    group.setUniversityName("MFTI");
+    group.setFaculty("Science");
+    group.setCourse(2);
     
+    std::cout << "name: " << group.getSurname()<< " " <<
+    "University: " <<  group.getUniversityName() << " " <<
+    "fucalty: " << group.getFaculty() << " " <<
+    "course: " << group.getCourse() << " \n";
     
+    //2. Создание обьекта с помощью конструктора с параметрами
+    GroupClass another_group("Konopleva", "MGU", "Programmer", 3);
+
+    std::cout << "name: " << another_group.getSurname()<< " " <<
+    "University: " <<  another_group.getUniversityName() << " " <<
+    "fucalty: " << another_group.getFaculty() << " " <<
+    "course: " << another_group.getCourse() << " \n";
+    
+
+    // 3. создание массива объектов (размерность массива 3 или 4
+    //    элемента),
+
+    GroupClass men_1;
+    GroupClass men_2;
+    GroupClass men_3;
+    GroupClass men_4;
+    
+    GroupClass array[4];
+    array[0] = men_1;
+    array[1] = men_2;
+    array[2] = men_3;
+    array[3] = men_4;
+    
+    //    4. инициализация свойств каждого объекта массива(исходные
+    //    данные вводятся с клавиатуры)
+    
+    for(int i=0; i < 4; i++) {
+        string surname;
+        string university;
+        string faculty;
+        int course;
+        
+        cout << "Введите фамилию: \n";
+        cin >> surname;
+        
+        cout << "Введите название университета: \n";
+        cin >> university;
+    
+        cout << "Введите название факультета: \n";
+        cin >> faculty;
+        
+        cout << "Введите курс: \n";
+        cin >> course;
+        
+        array[i].setSurname(surname);
+        array[i].setUniversityName(university);
+        array[i].setFaculty(faculty);
+        array[i].setCourse(course);
+    }
+    
+    //5.просмотр свойств каждого объекта
+    for(int i=0; i < 4; i++) {
+        cout << "Фамилия студента №" << i << ": " << array[i].getSurname() << ", ";
+        cout << "Университет студента №" << i << ": " << array[i].getUniversityName() << ", ";
+        cout << "Название факультета студента №" << i << ": " << array[i].getFaculty() << ", ";
+        cout << "Номер курса студента №" << i << ": " << array[i].getCourse() << "\n";
+    }
+    
+    //6. Определить количество студентов второго курса.
+    int numberOfStudents = 0;
+    
+    for(int i=0; i < 4; i++) {
+        if (array[i].getCourse() == 2) {
+            numberOfStudents ++;
+        }
+    };
+    
+    cout << "Число студентов второго курса: " << numberOfStudents << "\n";
+    cout << "Финал \n";
     
     return 0;
 }
